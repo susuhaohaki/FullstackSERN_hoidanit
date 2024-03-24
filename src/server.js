9ab20465c7f65fdf083f9066913449cf31aed306
susuhaohaki <nguyenhai250203@gmail.gmailcom>
2024-03-24 20:05:11 +0700

    #21 Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import Express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRoutes from "./route/web"
+import connectDB from "./config/connectDB"
 require("dotenv").config();
 
 
@@ -18,6 +19,7 @@ initWebRoutes(app);
 //port == undefind => 8081
 let port = process.env.PORT || 8081;
 
+connectDB();
 
 app.listen(port, () => {
     //callbak
